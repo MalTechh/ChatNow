6aca5850431bfd9e4953a0dbd398eca3c40914ba
MalTechh <109684312+MalTechh@users.noreply.github.com>
2022-11-17 19:28:43 -0600

    Delete login.js

--- login.js
+++
@@ -1,6 +0,0 @@
-const join = document.getElementById("join");
-join.addEventListener("click", () => {
-  const input = document.getElementById("name");
-  const username = input.value;
-});
-module.exports = { username };
